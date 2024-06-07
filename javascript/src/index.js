06bbeb6f144ce887ffea43778ca08aeb4806acb0
Nathami-n <georgenathan010@gmail.com>
2024-06-07 17:24:09 +0300

    Did counts using hashmap

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -64,7 +64,27 @@ const findTwoSum = (numbers) => {
 };
 
 
-console.log(findTwoSum([1,2, 3, 4, 5, 8]));
+let array = [1, 2, 3, 2, 1, 3, 4, 4, 5];
+
+let hashMap = {};
+
+const findOccurrence = (list) => {
+    array.forEach((element) => {
+        if(!hashMap[element]) {
+            hashMap[element] = 1;
+        } else {
+            hashMap[element] += 1;
+        }
+    });
+
+    console.log(hashMap);
+
+
+};
+
+findOccurrence();
+
+
 
 
 
